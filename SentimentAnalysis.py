96ab12b5aefb4d5427bcafebdee0388954e6d5ed
Sewar Yousef <71282585+SewarYousef@users.noreply.github.com>
2025-02-04 01:21:34 -0600

    Add files via upload

---
+++ SentimentAnalysis.py
@@ -0,0 +1,274 @@
+######sentiment analysis project
+
+import string
+import pandas as pd
+import numpy as np
+# import self as self
+import sklearn as sk
+import matplotlib as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
+import nltk
+import re
+import json
+import csv
+import tweepy as tw
+import os
+from nltk import text
+from sklearn.linear_model import LogisticRegression
+from wordcloud import WordCloud
+from textblob import TextBlob
+import sys
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import twython
+from twython import Twython
+
+plt.style.use('fivethirtyeight')
+################part1
+# nltk.download_shell()
+# API
+import oauth
+
+consumer_key = "EKyhdaxa4KDaxt5H1t2YC95A2"
+consumer_secret = "dEkQ7InFpwnysztkkMnw72AywCCzmrPhvAAbIzIwRyk64bnyPR"
+access_token = "1268668361275293697-0PIOK5bKUtW2BX50tHnSIQQgNyRCmg"
+access_token_secret = "vKt1qyOzEmeEXJ3be78gvrivB081Rs23PPUyiAzNfyZm2"
+
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+# Instantiate an object
+joker_tweets = Twython(consumer_key, consumer_secret)
+# Create our query
+query = {'q': '#jokermovie',
+         'count': 100,
+         'lang': 'en',
+         }
+# Search tweets
+dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}
+for status in joker_tweets.search(**query)['statuses']:
+    dict_['user'].append(status['user']['screen_name'])
+    dict_['date'].append(status['created_at'])
+    dict_['text'].append(status['text'])
+    dict_['favorite_count'].append(status['favorite_count'])
+# Structure data in a pandas DataFrame for easier manipulation
+df = pd.DataFrame(dict_)
+df.sort_values(by='favorite_count', inplace=True, ascending=False)
+print(df.head())
+print(df.describe())
+df.to_csv('/sentimentAnalysis/joker.csv')
+
+tweet_text = df['text']
+print(tweet_text.head())
+print(tweet_text.tail())
+print(df.shape)
+print('\n')
+
+Tweets = [line.rstrip() for line in df]
+print(len(Tweets))
+print(df.info())
+
+# Let's use groupby to use describe by sentiment
+print(df.groupby('text').describe())
+# print the first ten tweets
+for tweet_no, tweet in enumerate(df['text'][:10]):
+    print(tweet_no, tweet)
+    print('\n')
+# Let's make a new column to detect how long the tweets are:
+df['length'] = df['text'].apply(len)
+print(df.head())
+
+# Data Visualization
+df['length'].plot(bins=50, kind='hist')
+plt.show()
+
+# comment on the plot about long tweet:we found that the max length is 140
+print(df.length.describe())
+# lets try to find the tweet
+print(df[df['length'] == 140]['text'].iloc[0])
+
+# Clean the tweets:
+# nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+text = df['text']
+
+
+def clean(text):
+    text = re.sub(r'@[A-Za-z0–9]+', '', text)
+    text = re.sub(r'#', '', text)
+    text = re.sub(r'RT[\s]+', '', text)  # to remove retweets
+    text = re.sub(r'https?:\/\/\S+', '', text)
+    text = re.sub(r'[^\w\s]', '', text)
+    text = text.replace(text, text.lower())
+    return text
+
+
+# def delstopw(t):
+#     return [w for w in t if w not in stopwords.words('english')]
+
+
+df['text'] = df['text'].apply(clean)
+df['tokenized_text'] = df.apply(lambda row: nltk.word_tokenize(row['text']), axis=1)
+df['tokenized_text'] = df['tokenized_text'].apply(clean)
+print(df.head())
+readf = df.drop(['text'], axis=1)
+print(readf.head())
+
+# plot the word cloud
+import matplotlib.pyplot as plt
+
+allWords = ' '.join([tweet for tweet in df['text']])
+wordCloud = WordCloud(width=500, height=300, random_state=21, max_font_size=119).generate(allWords)
+plt.imshow(wordCloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+
+# subjectivity
+def getSubjectivity(text):
+    return TextBlob(text).sentiment.subjectivity
+
+
+df['subjectivity'] = df['text'].apply(getSubjectivity)
+
+
+# polarity
+def getPolarity(text):
+    return TextBlob(text).sentiment.polarity
+
+
+df['polarity'] = df['text'].apply(getPolarity)
+print(df.head())
+
+
+# create a function to count negative , positive and neutral
+def getAnalysis(score):
+    if score < 0:
+        return "negative"
+    elif score > 0:
+        return "positive"
+    else:
+        return "neutral"
+
+
+df['Analysis'] = df['polarity'].apply(getAnalysis)
+print(df.head())
+
+# plot polarity & subjectivity
+plt.figure(figsize=(8, 6))
+for i in range(0, df.shape[0]):
+    plt.scatter(df['polarity'][i], df['subjectivity'][i], color='green')
+plt.title('Sentiment Analysis')
+plt.xlabel('polarity')
+plt.ylabel('subjectivity')
+plt.show()
+# classify sentiment
+df.hist(column='length', by='Analysis', bins=50, figsize=(10, 8))
+plt.show()
+
+# positive tweets
+pos_tweets = df[df.Analysis == 'positive']
+pos_tweets = pos_tweets['text']
+print(pos_tweets)
+# number of pos_tweets
+print(round((pos_tweets.shape[0] / df.shape[0]) * 100, 1))
+# number of neg_tweets
+neg_tweets = df[df.Analysis == 'negative']
+neg_tweets = neg_tweets['text']
+print(neg_tweets)
+print(round((neg_tweets.shape[0] / df.shape[0]) * 100, 1))
+
+# value count
+c = df['Analysis'].value_counts()
+plt.xlabel('sentiment')
+plt.ylabel('counts')
+c.plot(kind='bar')
+plt.show()
+
+# part #2
+
+# cleaning is done
+
+# Part #3
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+
+# Vectorization and Model Selection
+bow_transformer = CountVectorizer(analyzer=clean).fit(df['tokenized_text'])
+tweet_bow = bow_transformer.transform(df['tokenized_text'])
+
+tfidf_transformer = TfidfTransformer().fit(tweet_bow)
+tweet_tfidf = tfidf_transformer.transform(tweet_bow)
+
+# Using naive bayes classifier
+classifier_model = MultinomialNB().fit(tweet_tfidf, df['tokenized_text'])
+all_predictions = classifier_model.predict(tweet_tfidf)
+print(classification_report(df['tokenized_text'], all_predictions))
+print(confusion_matrix(df['tokenized_text'], all_predictions))
+print(accuracy_score(df['tokenized_text'], all_predictions))
+
+
+pipeline = Pipeline([
+    ('bow', CountVectorizer(analyzer=clean)),
+    ('tfidf', TfidfTransformer()),
+    ('classifier', MultinomialNB()),
+])
+
+tf_vector = tweet_tfidf(np.array(df.iloc[:, 1]).ravel())
+X = tf_vector.transform(np.array(df.iloc[:, 1]).ravel())
+y = np.array(df.iloc[:, 0]).ravel()
+
+# Model Validation
+tweet_train, tweet_test, sentiment_train, sentiment_test = train_test_split(X, y, test_size=0.2, random_state=30)
+
+pipeline.fit(tweet_train, sentiment_train)
+predictions = pipeline.predict(tweet_test)
+print(classification_report(predictions, sentiment_test))
+
+# Using Logistic Regression Classifier
+classifier_model = LogisticRegression().fit(tweet_tfidf, df['tokenized_text'])
+all_predictions = classifier_model.predict(tweet_tfidf)
+print(classification_report(df['tokenized_text'], all_predictions))
+print(confusion_matrix(df['tokenized_text'], all_predictions))
+print(accuracy_score(df['tokenized_text'], all_predictions))
+
+pipeline = Pipeline([
+    ('bow', CountVectorizer(analyzer=clean)),
+    ('tfidf', TfidfTransformer()),
+    ('classifier', LogisticRegression()),
+])
+
+pipeline.fit(tweet_train, sentiment_train)
+predictions = pipeline.predict(tweet_test)
+print(classification_report(predictions, sentiment_test))
+
+
+rating = round((pos_tweets.shape[0] / (pos_tweets.shape[0] + neg_tweets.shape[0])) * 10)
+
+if 2.0 > rating >= 0:
+    print("Movie rating = 1 star")
+elif 2 <= rating < 3:
+    print("Movie rating = 2 star")
+elif 3 <= rating < 4:
+    print("Movie rating = 2.5 star")
+elif 4 <= rating < 5:
+    print("Movie_rating = 3 star")
+elif 5 <= rating < 6:
+    print("Movie rating = 3.5 star")
+elif 6 <= rating < 8:
+    print("Movie rating = 4 star")
+elif 8 <= rating < 9:
+    print('Movie rating = 4.5 star')
+elif 9 <= rating <= 10:
+    print("Movie rating = 5 star")
+
+# END OF THE CODE
